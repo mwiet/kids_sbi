Bug fixes in the pseudo-Cl calculation and in the noise subtraction

utils/add_salmo.py
@@ -61,8 +61,9 @@ def setup(options):
 
         if config['source_shifts']:
             lensing = np.array(str(config['doLensing']).split(' '), dtype = str)
-            nbLensFields = len(np.where(lensing == '0')[0])
-            if len(np.array(config['nOfZPath'].split(' '), dtype = str)) != nbLensFields:
+            config['nbLensFields'] = len(np.where(lensing == '0')[0])
+            config['nbSourceFields'] = len(np.where(lensing == '1')[0])
+            if len(np.array(config['nOfZPath'].split(' '), dtype = str)) != config['nbLensFields']:
                 raise Exception("""When source_shifts = T, you must only provide the nOfZPath for each lens sample.
                                 The path names leading to the nofzs of the source samples will be read from the shift_nz module.""")
         else:
@@ -218,6 +219,8 @@ def execute(block, config):
         block[config['out_name'], 'doVariableDepth'] = config['doVariableDepth']
         block[config['out_name'], 'denPrefix'] = '{0}/{1}_sample{2}/glass_denMap/{3}_denMap'.format(block['glass', 'map_folder'], block['glass', 'runTag'], block['glass', 'counter'], block['glass', 'prefix'])
         block[config['out_name'], 'lenPrefix'] = '{0}/{1}_sample{2}/glass_lenMap/{3}_lenMap'.format(block['glass', 'map_folder'], block['glass', 'runTag'], block['glass', 'counter'], block['glass', 'prefix'])
+        block[config['out_name'], 'nbLensFields'] = config['nbLensFields']
+        block[config['out_name'], 'nbSourceFields'] = config['nbSourceFields']
 
         mp = np.array(config['maskPath'].split(' '), dtype = str)
         maskPath = ['maskPath="{0} {1}"'.format(i, mp[i]) for i in range(len(mp))]

utils/fields.py
@@ -114,15 +114,15 @@ def execute(block, config):
 
     nside = config['nside']
     vd = block[config['in_name'], 'doVariableDepth']
-    lensing = np.array(str(block[config['in_name'], 'doLensing']).split(' '), dtype = str)
 
-    nbLensFields = len(np.where(lensing == '0')[0])
-    nbSourceFields = len(np.where(lensing == '1')[0])
+    nbLensFields = block[config['in_name'], 'nbLensFields']
+    nbSourceFields = block[config['in_name'], 'nbSourceFields']
 
     nz = np.array(str(block[config['in_name'], 'nOfZPath']).split(' '), dtype = str)
 
     mapping = {}
-    for i in np.unique(nz):
+    _, index = np.unique(nz, return_index=True)
+    for i in nz[np.sort(index)]:
         mapping[i] = np.where(nz == i)[0]
 
     joint_fields = list(mapping.values())   
@@ -139,6 +139,8 @@ def execute(block, config):
 
     print('Making shear maps...')
     tic = time.time()
+
+    print(joint_fields)
 
     #Shear
     tomo = 0
@@ -180,7 +182,7 @@ def execute(block, config):
                 print('  Computing alms for tomographic bin {0}...'.format(tomo+1))
                 shear = np.add.reduce(shear_group)
                 print('  -----')
-                alm.append([hp.sphtfunc.map2alm(shear.real, use_pixel_weights = True), hp.sphtfunc.map2alm(shear.imag, use_pixel_weights = True)])
+                alm.append(hp.sphtfunc.map2alm_spin([shear.real, -1*shear.imag], spin = 2, lmax = 4096))
                 del shear
             tomo += 1
     toc = time.time()
@@ -201,23 +203,27 @@ def execute(block, config):
         block[config['out_name'], "nbin_a"] = tomo
         block[config['out_name'], "nbin_b"] = tomo
         block[config['out_name'], "ell"] = np.arange(0, config['lmax']+1)
+        block[config['out_name'], 'nOfZPath'] = block[config['in_name'], 'nOfZPath']
         if nbSourceFields > 0:
             block[config['out_name'], 'n_density'] = block[config['in_name'], 'n_gal'][nbLensFields:]
             block[config['out_name'], 'sigma_e'] = block[config['in_name'], 'sigma_eps'][nbLensFields:]
+            block[config['out_name'], "nbLensFields"] = block[config['in_name'], "nbLensFields"]
+            block[config['out_name'], "nbSourceFields"] = block[config['in_name'], "nbSourceFields"]
 
         if str(vd) == '1':
             block[config['out_name'], 'a_n_density'] = block[config['in_name'], 'a_n_gal']
             block[config['out_name'], 'b_n_density'] = block[config['in_name'], 'b_n_gal']
             block[config['out_name'], 'a_sigma_e'] = block[config['in_name'], 'a_sigma_eps']
             block[config['out_name'], 'b_sigma_e'] = block[config['in_name'], 'b_sigma_eps']
+            block[config['out_name'], 'bin_depth'] = block[config['in_name'], 'bin_depth']
 
         print('Calculating angular power spectra...')
         tic = time.time()
         for i in range(tomo):
             for j in range(i+1):
                 print('  Getting shear Cls for bin {0} and bin {1}...'.format(i+1, j+1))
-                pcl = hp.alm2cl([alm[i][0], alm[i][1]], [alm[j][0], alm[j][1]], lmax = int(config['lmax']))[0] #EE mode
-                block[config['out_name'], 'bin_{0}_{1}'.format(i+1,j+1)] = pcl
+                pcls = hp.alm2cl([alm[i][0], alm[i][1]], [alm[j][0], alm[j][1]], lmax = int(config['lmax']))
+                block[config['out_name'], 'bin_{0}_{1}'.format(i+1,j+1)] = pcls[0]
         toc = time.time()
         print('Calculating angular power spectra took {0} seconds'.format(round(toc-tic, 3)))
     return 0

utils/remove_noise.py
@@ -5,7 +5,12 @@ import warnings
 def setup(options):
     config = {}
     config['where'] = options.get_string(option_section, "where") #only shear
-    config['fsky'] = options[option_section, 'fsky']
+
+    config['vd'] =  options.get_bool(option_section, "vd")
+
+    if config['vd']:
+        config['n_density'] = options[option_section, "n_density"]
+        config['sigma_eps'] = options[option_section, "sigma_eps"] 
 
     config['in_name'] = options.get_string(option_section, 'in_name')
     config['out_name'] = options.get_string(option_section, 'out_name')
@@ -16,29 +21,30 @@ def execute(block, config):
         nbin = block[config['in_name'], 'nbin']
         arcmin2_in_sphere =  60**6//100/np.pi
         try:
+            nbSourceFields = block[config['in_name'], "nbSourceFields"]
             n_density = block[config['in_name'], "n_density"]
             sigma_e = block[config['in_name'], "sigma_e"]
+            if len(sigma_e) != nbSourceFields:
+                print('WARNING: Sigma_eps and nbSourceFields incompatible!')
+
+            nbLensFields = block[config['in_name'], "nbLensFields"]
+            nofz_paths = block[config['in_name'], "nOfZPath"]
+            nofz_paths = np.array(nofz_paths.split(' '), dtype = str)[nbLensFields:]
+            #Make sure each tomographic bin is only included once
+            _, index = np.unique(nofz_paths, return_index=True)
+            if len(index) != 0:
+                n_density = n_density[np.sort(index)]
+                sigma_e = sigma_e[np.sort(index)]
+            print('Found uniform shape and shot noise values...')
         except:
+            nbSourceFields = 0
             n_density = np.array([])
             sigma_e = np.array([])
+            print('No uniform shape and shot noise values found.')
 
-        try:
-            a_n_density = block[config['in_name'], "a_n_density"]
-            b_n_density = block[config['in_name'], "a_n_density"]
-            a_sigma_e = block[config['in_name'], "a_sigma_e"]
-            b_sigma_e = block[config['in_name'], "b_sigma_e"]
-            bin_depth = block['salmo', "bin_depth"]
-            vd = True
-        except:
-            vd = False
-            pass
-
-        if vd:
-            bin_mean = bin_depth[:-1] + 0.5*(bin_depth[1:] - bin_depth[:-1])
-            n = np.mean(a_n_density*bin_mean[:, None] + b_n_density, axis = 0)
-            s = np.mean(a_sigma_e*bin_mean[:, None] + b_sigma_e, axis = 0)
-            n_density = np.concatenate((n_density, n))
-            sigma_e = np.concatenate((sigma_e, s))
+        if config['vd']:
+            n_density = np.array(config['n_density'])
+            sigma_e = np.array(config['sigma_eps'])
         
         n_density = n_density*arcmin2_in_sphere/(4*np.pi)
         ell =  block[config['in_name'], 'ell']
@@ -50,7 +56,8 @@ def execute(block, config):
                 if i == j:
                     with warnings.catch_warnings():
                         warnings.filterwarnings("ignore", category=RuntimeWarning)
-                        noise = sigma_e[i]**2/2*n_density[i]
+                        noise = (sigma_e[i]**2)/(2*n_density[i])
+                    print('Removing ({0}^2)/(2*{1} arcmin^-2) of angular power in {2}-{2}'.format(round(sigma_e[i], 2), round(n_density[i]*4*np.pi/arcmin2_in_sphere, 2), i+1))
                 else:
                     noise = 0
                 shear_cl -= noise

utils/shift_nz.py
@@ -33,7 +33,8 @@ def execute(block, config):
 
     new_files = []
     if config['doVariableDepth'] == '0':
-        nz_unique = np.unique(nz)
+        _, index = np.unique(nz, return_index=True)
+        nz_unique = nz[np.sort(index)]
         mapping = {}
         for i, file in enumerate(nz_unique):
             mapping[file] = np.where(nz == file)[0]