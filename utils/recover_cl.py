b946e73d69445bdbdee4906cc336fb43e426a6bf
mwiet <ucapmhw@ucl.ac.uk>
2023-03-30 18:54:39 +0100

    Fixed small bug

--- utils/recover_cl.py
+++ utils/recover_cl.py
@@ -1,6 +1,7 @@
 from cosmosis.datablock import option_section, names, BlockError
 import numpy as np
 import healpy as hp
+from scipy.interpolate import interp1d
 
 def setup(options):
     config = {}
@@ -13,7 +14,7 @@ def setup(options):
     config['in_name'] = options.get_string(option_section, 'in_name')
     config['out_name'] = options.get_string(option_section, 'out_name')
 
-    if config['mm'][-4:] != '.npz':
+    if config['mm_mask'][-4:] != '.npz':
         raise Exception('Only numpy .npz files are supported for the mixing matrix.')
     config['out'] = options.get_string(option_section, "out") #only shear
     return config
@@ -21,20 +22,13 @@ def setup(options):
 def execute(block, config):
     if config['out'] == 'shear':
         print('Reading in shear mixing matrix: {0}'.format(config['mm']))
-        mm = np.load(config['mm'])['arr_0']
+        mm = np.load(config['mm_mask'])['arr_0']
         nbin = block[config['in_name'], 'nbin']
 
         ell = block[config['in_name'], 'ell']
-        l_min = int(np.min(ell))
-        l_max = int(np.max(ell))
-
-        ell_new = ell[(l_max > ell) & (ell >= l_min)]
-
-        if np.all((ell_new[1:] - ell_new[:-1]) == 1.0):
-            print('Given Cls are calculated at interger values of ell already.')
-        else:
-            raise Exception('Given Cls are NOT calculated at interger values of ell.')
-            
+        l_min = int(round(np.min(ell), 0))
+        l_max = int(round(np.max(ell), 0))
+           
         block[config['out_name'], "is_auto"] = 'True'
         block[config['out_name'], "sample_a"] = 'source'
         block[config['out_name'], "sample_b"] = 'source'
@@ -49,8 +43,10 @@ def execute(block, config):
         for i in range(nbin):
             for j in range(i+1):
                 shear_cl = block[config['in_name'], 'bin_{0}_{1}'.format(i+1, j+1)]
+                cl_interp = interp1d(ell, shear_cl)
+                cl = cl_interp(np.arange(l_min, l_max+1))
                 mm_sampling = np.load(config['mm_sampling_prefix'] + '_{0}_{1}.npz'.format(i+1, j+1))['arr_0']
-                block[config['out_name'], 'bin_{0}_{1}'.format(i+1,j+1)] = pixel_window[l_min:l_max+1]**2 * mm[l_min:l_max+1, l_min:l_max+1].dot(mm_sampling[l_min:l_max+1, l_min:l_max+1].dot(shear_cl[l_min:l_max+1]))
+                block[config['out_name'], 'bin_{0}_{1}'.format(i+1,j+1)] = pixel_window[l_min:l_max+1]**2 * mm[l_min:l_max+1, l_min:l_max+1].dot(mm_sampling[l_min:l_max+1, l_min:l_max+1].dot(cl))
                 counter += 1
         
     else:
